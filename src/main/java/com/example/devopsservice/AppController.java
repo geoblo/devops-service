647e627088a9e356d3872a561fa22e5cf743b83a
goni.kim <geoblo@naver.com>
2026-09-10 16:53:34 +0900

    cicd 3rd

--- src/main/java/com/example/devopsservice/AppController.java
+++ src/main/java/com/example/devopsservice/AppController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppController {
     @GetMapping("/")
     public String index() {
-        return "Good Day 1~2~3";
+        return "Good Day!!!!!!!!!!!!!!!!!!!!";
     }
 }
